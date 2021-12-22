25bdc42fb61cd1b0bd88a95883500c73afaf03cc
geduardcatalindev <geduardcatalindev@gmail.com>
2021-12-22 16:41:23 +0200

    - initial commit;

---
+++ src/Controller/TestController.php
@@ -0,0 +1,21 @@
+<?php
+// src/Controller/TestController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController
+{
+    /**
+     * @Route("/test", name="test")
+     */
+    public function test(): JsonResponse
+    {
+        return new JsonResponse(
+            [
+                'key' => 'value'
+            ]
+        );
+    }
+}
